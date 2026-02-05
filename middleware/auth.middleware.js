6d097a40c791e715d06dc00f602b204a6ecea123
Vishal Gupta <your-email@example.com>
2026-02-06 00:41:55 +0530

    middleware chnagesssss

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,7 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies?.accessToken;
+  let token = req.cookies?.accessToken;
+
+  if (!token && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
   if (!token) return res.status(401).json({ msg: "Unauthorized" });
 
   try {
